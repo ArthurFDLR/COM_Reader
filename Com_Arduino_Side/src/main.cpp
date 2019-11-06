5595864b40b0f4d977f7cdca0938ee3d6526652b
Arthur FINDELAIR <41486077+ArthurFDLR@users.noreply.github.com>
2019-11-06 18:54:23 +0100

    Add files via upload

---
+++ Com_Arduino_Side/src/main.cpp
@@ -0,0 +1,15 @@
+#include "Arduino.h"
+
+#define BAUD 9600
+#define DELAY_TIME 100
+
+const char* helloWorld = "Hello, world!";
+
+void setup() {
+  Serial.begin(BAUD);
+}
+
+void loop() {
+  Serial.println(helloWorld);
+  delay(DELAY_TIME);
+}
